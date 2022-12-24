90020d8c5b96cbc52c24995317ce55292ccb30c5
gowthami145 <119786338+gowthami145@users.noreply.github.com>
2022-12-24 09:36:35 +0530

    #include<stdio.h> #include<math.h> int main() {     int s;     scanf("%d",&s);     float sa,vol;     sa=6*s*s;     vol=pow(s,3);     printf("Surface area = %.f and Volume = %.f",sa,vol);
    }

---
+++ surface_area_and_volume_of_a_cube.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int s;
+    scanf("%d",&s);
+    float sa,vol;
+    sa=6*s*s;
+    vol=pow(s,3);
+    printf("Surface area = %.f and Volume = %.f",sa,vol);
+    
+    
+}
