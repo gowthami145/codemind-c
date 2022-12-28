ce5817e568a2829bc214ab188b0da8768032f5ba
gowthami145 <119786338+gowthami145@users.noreply.github.com>
2022-12-28 21:37:05 +0530

    #include<stdio.h> int main() {     int n,q,s=0,sn1,sn2,r,rsn2;     scanf("%d",&n);     sn1=n*n;     q=n;     while(q!=0)     {         r=q%10;         s=s*10+r;         q=q/10;     }     sn2=s*s;     q=sn2;     s=0;     while(q!=0)     {         r=q%10;         s=s*10+r;         q=q/10;     }     rsn2=s;     if(sn1==rsn2)     {         printf("True");     }     else     {         printf("False"); } }

--- Adam_number.c
+++ Adam_number.c
@@ -6,27 +6,27 @@ int main()
     sn1=n*n;
     q=n;
     while(q!=0)
-{
-    r=q%10;
-    s=s*10+r;
-    q=q/10;
-}
-sn2=s*s;
-q=sn2;
-s=0;
-while(q!=0)
-{
-    r=q%10;
-    s=s*10+r;
-    q=q/10;
-}
-rsn2=s;
-if(sn1==rsn2)
-{
-    printf("True");
-}
-else
-{
-    printf("False");
+    {
+        r=q%10;
+        s=s*10+r;
+        q=q/10;
+    }
+    sn2=s*s;
+    q=sn2;
+    s=0;
+    while(q!=0)
+    {
+        r=q%10;
+        s=s*10+r;
+        q=q/10;
+    }
+    rsn2=s;
+    if(sn1==rsn2)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
 }
 }
